7c30dc67d2099c49b449aeb73364a6891d31e15a
aaf40 <aaf40@txstate.edu>
2024-12-01 17:12:25 -0600

    changed all subi to addi for compilation

--- src/codegen.c
+++ src/codegen.c
@@ -814,10 +814,10 @@ static int generateFunctionCall(tree* node) {
         }
         
         // Final stack adjustment after all arguments (args + 1 for return address)
-        emitInstruction("\tsubi $sp, $sp, %d", (args->numChildren + 1) * 4);
+        emitInstruction("\addi $sp, $sp, -%d", (args->numChildren + 1) * 4);
     } else {
         // No arguments, just adjust for return address
-        emitInstruction("\tsubi $sp, $sp, 4");
+        emitInstruction("\addi $sp, $sp, -4");
     }
     
     // Generate function call
@@ -962,19 +962,19 @@ static void generateFunctionPrologue(const char* functionName, int numLocalVars)
     emitInstruction("\t# Setting up FP");
     emitInstruction("\tsw $fp, ($sp)");
     emitInstruction("\tmove $fp, $sp");
-    emitInstruction("\tsubi $sp, $sp, 4\n");
+    emitInstruction("\addi $sp, $sp, -4\n");
     
     // Save registers
     emitInstruction("\t# Saving registers");
     for (int i = 0; i <= 7; i++) {
         emitInstruction("\tsw $s%d, ($sp)", i);
-        emitInstruction("\tsubi $sp, $sp, 4");
+        emitInstruction("\addi $sp, $sp, -4");
     }
     
     // Allocate space for local variables
     if (numLocalVars > 0) {
         emitInstruction("\n\t# Allocate space for %d local variables.", numLocalVars);
-        emitInstruction("\tsubi $sp, $sp, %d", numLocalVars * 4);
+        emitInstruction("\addi $sp, $sp, -%d", numLocalVars * 4);
     }
     printf("\n");
 }
